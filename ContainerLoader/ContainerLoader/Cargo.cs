47de2cc77eb8e4d1e0c198b77ee32fd8b087b33b
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2025-03-22 17:35:55 +0100

    Fix field IsHazardous typo

--- ContainerLoader/ContainerLoader/Cargo.cs
+++ ContainerLoader/ContainerLoader/Cargo.cs
@@ -5,6 +5,8 @@ public abstract class Cargo(string name, double weight)
     public string Name { get; protected set; } = name;
     public double Weight { get; protected set; } = weight;
     
-    public virtual bool IsHazardous =>
-        Enum.TryParse<HazardousCargo>(Name, ignoreCase: true, out _);
+    public virtual HazardousCargo? HazardType => null;
+
+    public virtual bool IsHazardous => HazardType.HasValue;
+    
 }
