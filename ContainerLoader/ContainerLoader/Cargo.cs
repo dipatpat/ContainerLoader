3dee387b5166cbae2277f83f081974c00561c2a9
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2025-03-22 15:42:20 +0100

    Use Enum Hazardous Cargo to retrieve the value of IsHazardous property

--- ContainerLoader/ContainerLoader/Cargo.cs
+++ ContainerLoader/ContainerLoader/Cargo.cs
@@ -5,5 +5,6 @@ public abstract class Cargo(string name, double weight)
     public string Name { get; protected set; } = name;
     public double Weight { get; protected set; } = weight;
     
-    public virtual bool IsHazardous => false;
+    public virtual bool IsHazardous =>
+        Enum.TryParse<HazardousCargo>(Name, ignoreCase: true, out _);
 }
---
+++ ContainerLoader/ContainerLoader/OrdinaryCargo.cs
@@ -0,0 +1,6 @@
+namespace ContainerLoader;
+
+public class OrdinaryCargo(string name, double weight) : Cargo(name, weight) 
+{
+    
+}
